522cb1bb4f9e50ed7ce4fafbae00d9c638469e47
Almaz <gleemand@gmail.com>
2021-10-01 18:36:51 +0300

    Some fixes

--- info.php
+++ info.php
@@ -87,20 +87,25 @@ function displayHtml($titleData, $data)
     echo "<table class='styled-table' border style='border-collapse:collapse; width:500px; text-align:center'>";
 
     if ($titleData == 'Formas de Pago') {
-        echo '<p>Copy siigo-id to "Description" field of appropriate payment method at CRM <a target="_blank" href="'
+        echo '<p>Copy "ID" to "Description" field of appropriate payment method at CRM <a target="_blank" href="'
         . $simlaUrl
         . 'admin/payment-types">here</a>.</p>';
     }
     echo $titleData != 'Usuarios'
-        ? "<thead><tr>" . "<td>siigo-id" . "<td>name" . "</tr></thead>"
-        : "<thead><tr>" . "<td>siigo-id" . "<td>e-mail" . "<td>active" . "</tr></thead>";
+        ? "<thead><tr>" . "<td>ID" . "<td>Name" . "</tr></thead>"
+        : "<thead><tr>" . "<td>ID" . "<td>User" . "<td>Active" . "</tr></thead>";
 
     foreach ($data as $value) {
         echo "<tr>";
-        echo "<td>siigo-{$value['id']}</td>";
+
+        if ($titleData == 'Formas de Pago') {
+            echo "<td>siigo-{$value['id']}</td>";
+        } else {
+            echo "<td>{$value['id']}</td>";
+        }
 
         if ($titleData == 'Usuarios') {
-            echo "<td>{$value['email']}</td>";
+            echo "<td>{$value['identification']}</td>";
             echo "<td>{$value['active']}</td>";
         } else {
             echo "<td>{$value['name']}</td>";
@@ -115,56 +120,58 @@ function displayHtml($titleData, $data)
 function html($place)
 {
     if ($place === 'begin') {
-        return '<!DOCTYPE html>
-        <html>
-        <head>
-        <style>
-        * {
-            font-family: sans-serif;
-            text-align: center;
-        }
-        p {
-            font-size: 0.9em;
-        }
-        h3 {
-            font-size: 1.2em;
-        }
-        .styled-table {
-            border-collapse: collapse;
-            font-size: 0.9em;
-            font-family: sans-serif;
-            min-width: 400px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-            margin-left: auto;
-            margin-right: auto;
-        }
-        .styled-table thead tr {
-            background-color: #009879;
-            color: #ffffff;
-            font-weight: bold;
-        }
-        .styled-table th,
-        .styled-table td {
-            padding: 12px 15px;
-        }
-        .styled-table tbody tr {
-            border-bottom: 1px solid #dddddd;
-        }
+        return
+            '<!DOCTYPE html>
+                <html>
+                    <head>
+                        <style>
+                            * {
+                                font-family: sans-serif;
+                                text-align: center;
+                            }
+                            p {
+                                font-size: 0.9em;
+                            }
+                            h3 {
+                                font-size: 1.2em;
+                            }
+                            .styled-table {
+                                border-collapse: collapse;
+                                font-size: 0.9em;
+                                font-family: sans-serif;
+                                min-width: 400px;
+                                box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+                                margin-left: auto;
+                                margin-right: auto;
+                            }
+                            .styled-table thead tr {
+                                background-color: #009879;
+                                color: #ffffff;
+                                font-weight: bold;
+                            }
+                            .styled-table th,
+                            .styled-table td {
+                                padding: 12px 15px;
+                            }
+                            .styled-table tbody tr {
+                                border-bottom: 1px solid #dddddd;
+                            }
 
-        .styled-table tbody tr:nth-of-type(even) {
-            background-color: #f3f3f3;
-        }
+                            .styled-table tbody tr:nth-of-type(even) {
+                                background-color: #f3f3f3;
+                            }
 
-        .styled-table tbody tr:last-of-type {
-            border-bottom: 2px solid #009879;
-        }
-        </style>
-        </head>
-        <body>
-        <h3>Data provided by Siigo</h3>
-        <br>';
+                            .styled-table tbody tr:last-of-type {
+                                border-bottom: 2px solid #009879;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h3>Data provided by Siigo</h3>
+                        <br>';
     } else if ($place === 'end') {
-        return '</body>
-        </html>';
+        return
+                    '</body>
+                </html>';
     }
 }
